62a7c9dec8faf2315645793c8609b73e7c7c8f24
Grigor Safaryan <grigor.safaryan@ekeng.am>
2023-09-14 12:36:37 +0400

    adding

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import View from "./View/View.jsx";
+import "./index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<View />);
---
+++ src/view/View.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/header/header";
+
+const View = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default View;
