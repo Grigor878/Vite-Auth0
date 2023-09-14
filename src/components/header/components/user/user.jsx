ea94f47e3259136aa31a96c9c5beda6de8d1305c
Grigor Safaryan <grigor.safaryan@ekeng.am>
2023-09-14 15:58:55 +0400

    needed browserRouter

---
+++ src/components/header/components/user/user.jsx
@@ -0,0 +1,5 @@
+import styles from "./user.module.scss";
+
+export const User = () => {
+  return <div className={styles.user}>Template</div>;
+};
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,17 @@
+import { NavLink } from "react-router-dom";
 import { Auth } from "./components/auth/auth";
+import { User } from "./components/user/user";
+import logo from "../../assets/logo.png";
 import styles from "./header.module.scss";
 
 export const Header = () => {
-  return <div className={styles.header}>
-    <Auth/>
-  </div>;
+  return (
+    <header className={styles.header}>
+      <NavLink to="/" onClick={() => window.scrollTo(0, 0)}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+      </NavLink>
+      <Auth />
+      <User />
+    </header>
+  );
 };
