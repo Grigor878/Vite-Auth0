a4513d38adf80954688e41d7e5c96846f459c5b2
Grigor Safaryan <grigor.safaryan@ekeng.am>
2025-03-14 15:18:18 +0400

    new helpers,formatters and regexes

--- src/utils/formaters.js
+++ src/utils/formaters.js
@@ -1,4 +1,9 @@
 // Object to array of keys
 export function getLanguageKeys(languages) {
-    return Object.keys(languages)
-  }
+  return Object.keys(languages)
+}
+
+// get current year
+export function getCurrentYear() {
+  return new Date().getFullYear();
+}
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17,3 +17,50 @@ export function isEmptyObject(obj) {
 export function isNotEmptyObject(obj) {
     return Object?.keys(obj)?.length > 0 || obj?.constructor !== Object;
 }
+
+// Overflow by props
+export function overflowCheck(props) {
+    props ? (document.body.style.overflow = "hidden")
+        : (document.body.style.overflow = "auto");
+}
+
+// Download file
+import { success } from "../components/toast/toast";
+
+export function downloadFiles(data, filename) {
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(new Blob([data]));
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    success("Ներբեռնումն ավարտված է։")
+}
+
+// Download file
+export function downloadXmls(data, filename) {
+    const url = window.URL.createObjectURL(data);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+    success("Ներբեռնումն ավարտված է։")
+}
+
+// Check column include HTML
+import { htmlRegex } from "./regex";
+
+export function isHTML(str) {
+    return htmlRegex?.test(str)
+}
+
+// Check column include HTML
+import { urlRegex } from "./regex";
+
+export function isURL(str) {
+    return urlRegex?.test(str)
+}
